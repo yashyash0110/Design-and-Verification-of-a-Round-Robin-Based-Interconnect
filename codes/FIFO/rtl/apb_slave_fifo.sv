8a6d900acc319fe3530164fc9ceefed1625e1f32
G Yashwanth Kumar <102477190+yashyash0110@users.noreply.github.com>
2025-05-06 11:23:52 +0530

    Update and rename codes/FIFO/apb_slave_fifo.sv to codes/FIFO/rtl/apb_slave_fifo.sv

--- codes/FIFO/rtl/apb_slave_fifo.sv
+++ codes/FIFO/rtl/apb_slave_fifo.sv
@@ -1,33 +1,56 @@
+// Code your design here
 `define DEPTH_FIFO 8
-module apb_slave_fifo(clk,reset,push_in,pop_in,push_wdata_in,push_addr_in,pop_wdata_out,pop_addr_out,full_o,empty_o,data_in_ack,write,arb_write);
+
+/*Data that is popped out is sent as a packet to the arbiter along with REQ signal which specifies that the transaction needs to be serviced by the arbiter*/
+typedef struct {
+  logic [31:0] wdata;
+  logic [31:0] addr;
+  logic write;
+} packet_out;
+
+
+module apb_slave_fifo(
   //From APB Slave
-  input logic clk;
-  input logic reset;
-  input logic push_in;
-  input logic write;
+  input logic clk,
+  input logic reset,
+  input logic push_in,
+  input logic [31:0] push_wdata_in,
+  input logic [31:0] push_addr_in,
+  input logic write,
   
-  input logic [31:0] push_wdata_in;
-  input logic [31:0] push_addr_in;
   //To APB Slave
-  output logic data_in_ack;
-  output logic full_o;
+  output logic data_in_ack,
+  output logic full_o,
+  
+  //From Arbiter
+  input logic pop_in, //GRANT from the Arbiter drives this
+  
+  //To Arbiter
+  output packet_out fifo_packet_out,
+  output logic arb_req
+  /*When the data is popped out, that means it is the transaction request   to be sent to the abiter. The data is popped out only when the arbiter  completes the previous transactions right*/
+);
   
   //To arbiter
-  output logic [31:0] pop_wdata_out;
-  output logic [31:0] pop_addr_out;
-  output logic arb_write;
-  //From arbiter
-  input logic pop_in; //Handle this case
-  input logic empty_o;
+  logic [31:0] pop_wdata_out;
+  logic [31:0] pop_addr_out;
+  logic arb_write;
+  logic fifo_empty;
 
   //Output Assignments
   assign arb_write = write;
   assign data_in_ack = push_in && !full_o;
+  assign fifo_packet_out = '{wdata : pop_wdata_out,
+                             addr  : pop_addr_out,
+                             write : arb_write
+                            };
+  
+  assign arb_req = ~fifo_empty; // Only request when data is there
   
   typedef enum logic [1:0] {
     PUSH=2'b01,
     POP=2'b10,
-    BOTH=2'b11}fifo_state;
+    BOTH=2'b11 } fifo_state;
   
   localparam PTR_W = $clog2(`DEPTH_FIFO); //Pointers width in bits
   
@@ -69,6 +92,12 @@ module apb_slave_fifo(clk,reset,push_in,pop_in,push_wdata_in,push_addr_in,pop_wd
         full_o = (nxt_wr_ptr_w == rd_ptr_w | nxt_wr_ptr_a == rd_ptr_a) ? 1 : 0;
       else
         full_o = (nxt_wr_ptr_a == rd_ptr_a) ? 1 : 0;
+    end
+  
+  //Handle Empty condition of FIFO
+  always_comb
+    begin
+      fifo_empty = (wr_ptr_a == rd_ptr_a && wr_ptr_w == rd_ptr_w) ? 1 : 0;
     end
   
   //Pointer logic for push and pop
@@ -141,9 +170,8 @@ module apb_slave_fifo(clk,reset,push_in,pop_in,push_wdata_in,push_addr_in,pop_wd
         BOTH:
           begin
             if(write) //WRITE
-              begin
-                
-                /*PUSH*/
+              begin 
+                //PUSH//
                 nxt_fifo_wdata_in = push_wdata_in;
                 nxt_fifo_addr_in = push_addr_in;
                 //Manipulate the write pointer of FIFO
@@ -158,7 +186,7 @@ module apb_slave_fifo(clk,reset,push_in,pop_in,push_wdata_in,push_addr_in,pop_wd
                 else
                   nxt_wr_ptr_a = wr_ptr_a + 1'b1;
                 
-                /*POP*/
+                //POP//
                 pop_wdata_out = fifo_wdata_regs[rd_ptr_w];
                 pop_addr_out = fifo_addr_regs[rd_ptr_a];
                 //Manipulate the write pointer of FIFO
@@ -177,7 +205,7 @@ module apb_slave_fifo(clk,reset,push_in,pop_in,push_wdata_in,push_addr_in,pop_wd
             else //READ
               begin
                 
-                /*PUSH*/
+                //PUSH//
                 nxt_fifo_addr_in = push_addr_in;
                 //Manipulate the write pointer of FIFO
                 if(wr_ptr_a == PTR_W'(`DEPTH_FIFO-1))
@@ -185,7 +213,7 @@ module apb_slave_fifo(clk,reset,push_in,pop_in,push_wdata_in,push_addr_in,pop_wd
                 else
                   nxt_wr_ptr_a = wr_ptr_a + 1'b1;
                 
-                /*POP*/
+                //POP//
                 pop_addr_out = fifo_addr_regs[rd_ptr_a];
               	//Manipulate the write pointer of FIFO
              	if(rd_ptr_a == PTR_W'(`DEPTH_FIFO-1))
@@ -219,4 +247,5 @@ module apb_slave_fifo(clk,reset,push_in,pop_in,push_wdata_in,push_addr_in,pop_wd
       else //READ
         fifo_addr_regs[wr_ptr_a] <= nxt_fifo_addr_in;
     end
+  
 endmodule
